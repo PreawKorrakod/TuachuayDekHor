19f4f22c49e2a36ca2bd514366fb543b1322ed21
korrakod <korrakod2003@gmail.com>
2023-09-15 00:48:14 +0700

    second

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
 import Home from "./pages/Home";
+import Report from "./pages/Report";
 import { useEffect } from "react";
 
 function App() {
@@ -50,6 +51,7 @@ function App() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<LogIn />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/report" element={<Report />} />
     </Routes>
   );
 }
---
+++ src/pages/Report.js
@@ -0,0 +1,15 @@
+import "./Report.css";
+import { Link } from "react-router-dom";
+const Report = () =>{
+    return (
+        <div className="report">
+            <h1 id="report-h1">Report</h1>
+            <div className="input-report">
+                <textarea rows={10} id="typereport" type="text" placeholder="type your problem..." />
+                <Link id="send-1"to="/home" className="button">Send Report</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Report;
