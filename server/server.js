0315fbeab6c3618492dea202e241db5e453b19d5
pumNicharee <s6501012610041@email.kmutnb.ac.th>
2023-09-24 02:05:05 +0700

    pum_backend_2

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors');
 const mysql = require('mysql');
 const { log } = require('console');
+const axios = require('axios').default;
+
 
 const app = express();
 
@@ -24,123 +26,82 @@ connection.connect((err) => {
     console.log('MySQL succesfully connected!');
 })
 
-//CREATE Routes
-app.post("/create",async(req,res) => {
-    const {name,email,password,photo} = req.body;
-    
-    try{
-        connection.query(
-            "INSERT INTO users(username,avatar,email,password) VALUES(?,?,?,?)",
-            [name,photo,email,password],
-            (err,results,fields) => {
-                if (err) {
-                    console.log("Error while inserting a user into the database",err);
-                    return res.status(400).send();
-                }
-                return res.status(201).json({message: "New user successfully created!"})
-            }
-        )
-    }catch(err){
-        console.log(err);
-        return res.status(500).send();
-    }
+
+// Check 
+app.get("/check/:username&/check/:email",async (req,res) => {
+    const {username, email,password,confirmpassword} = req.body;
+    const sql = "SELECT * FROM users WHERE username = ? or email = ?"
+    connection.query(sql,[username,email],(err,data) => {
+        // console.log(data.map(item => item.email).toString().includes(req.body.email,0));
+        if (err){
+            return res.status(400).json(err.sqlMessage);
+        }
+        if (data.map(item => item.username).toString() == username && data.map(item => item.email).toString() == email) {
+            return res.status(200).json({status: "account" ,msg:"You already have an account"});
+        }
+        if (data.map(item => item.username).toString().includes(req.body.username,0)){
+            return res.status(200).json({status: "fail_username" ,msg:"This username has already use!"});
+        }
+        if (data.map(item => item.email).toString().includes(req.body.email,0)){
+            return res.status(200).json({status: "fail_email" ,msg:"This email has already use!"});
+        }
+        else{
+            return res.status(200).json({status: "create" ,msg:"You can create an account"});
+        }
+    })        
 })
 
-//READ
-app.get("/read",async(req,res) => {
-    try{
-        connection.query("SELECT * FROM users",(err,results,fields) =>{
-            if (err){
-                console.log(err);
-                return res.status(400).send();  
-            }
-            res.status(200).json(results)
-        })
-    } catch(err){
-        console.log(err);
-        return res.status(500).send();
-    }
-})
-
-//READ single users from db
-app.get("/read/single/:email",async(req,res) => {
-    const email = req.params.email;
-    try{
-        connection.query("SELECT * FROM users WHERE email = ?",[email],(err,results,fields) =>{
-            if (err){
-                console.log(err);
-                return res.status(400).send();  
-            }
-            res.status(200).json(results)
-        })
-    } catch(err){
-        console.log(err);
-        return res.status(500).send();
-    }
-})
-
-//UPDATE data
-app.patch("/update/:email", async (req,res) => {
-    const email = req.params.email;
-    const newPassword = req.body.newPassword;
-    try{
-        connection.query("UPDATE users SET password = ? WHERE email = ?",[newPassword,email],(err,results,fields) =>{
-            if (err){
-                console.log(err);
-                return res.status(400).send();  
-            }
-            res.status(200).json({message: "User password updated successfully!"})
-        })
-    } catch(err){
-        console.log(err);
-        return res.status(500).send();
-    }
-})
-
-//DELETE
-app.delete("/delete/:email",async (req,res) => {
-    const email = req.params.email;
-    try{
-        connection.query("DELETE FROM users WHERE email = ?",[email],(err,results,fields) =>{
-            if (err){
-                console.log(err);
-                return res.status(400).send();  
-            }
-            if (results.affectedRows == 0) {
-                return res.status(404).json({message: "No user witn that email!"}); 
-            }
-            return res.status(200).json({message: "User deleted successfully!"})
-        })
-    } catch(err){
-        console.log(err);
-        return res.status(500).send();
-    }
-})
 
 //signup
 app.post("/signup",async (req,res) => {
     const sql = "INSERT INTO users(username,email,password) VALUES (?, ?, ?)";
     const {username, email, password, confirmpassword} = req.body;
-    if (password == confirmpassword){
-        connection.query(sql,[username, email, password],(err,data) => {
-            if(err) {
-                return res.status(400).json(err.sqlMessage);
+    // if (password == confirmpassword){
+    //     connection.query(sql,[username, email, password],(err,data) => {
+    //         if(err) {
+    //             return res.status(400).json(err.sqlMessage);
+    //         }
+    //         return res.status(200).json("This data is complete in table");
+    //     })
+    // }
+    // else {
+    //     return res.status(400).json("password must be the same");
+    // }
+    axios.get('https://localhost:3300/check/')
+    .then(status =>{
+        if (status.status == create){
+            if (password == confirmpassword){
+                connection.query(sql,[username, email, password],(err,data) => {
+                    if(err) {
+                        return res.status(400).json(err.sqlMessage);
+                    }
+                    return res.status(200).json("This data is complete in table");
+                })
             }
-            return res.status(200).json("This data is complete in table");
-        })
-    }
-    else {
-        return res.status(400).json("password must be the same");
-    }
+            else {
+                return res.status(400).json("password must be the same");
+            }
+        }
+        if (status.status == account){
+            return res.status(200).json({status: "account" ,msg:"You already have an account"});
+        }
+        if (status.status = fail_username){
+            return res.status(200).json({status: "fail_username" ,msg:"This username has already use!"});
+        }
+        if (status.status == fail_email){
+            return res.status(200).json({status: "fail_email" ,msg:"This email has already use!"});
+        }
+
+    })
 })
 
 //login
 app.get("/login",async (req,res) => {
     const sql = "SELECT * FROM users WHERE email = ? AND password = ?";
     connection.query(sql,[req.body.email,req.body.password],(err,data) =>{
-        console.log(data);
+        // console.log(data);
         if (err) {
-            return res.json(err.sqlMessage);
+            return res.status(400).json(err.sqlMessage);
         }
         if (data.length > 0){
             return res.json("Success");
