8963200a1313eb0918e6840b9f09a7947195c58d
korrakod <korrakod2003@gmail.com>
2023-10-04 17:33:25 +0700

    twentyone

--- src/pages/WriteBlog.js
+++ src/pages/WriteBlog.js
@@ -3,13 +3,12 @@ import React from 'react';
 import NavHead from "../component/Nav";
 import AddPost from "../component/AddPost";
 import {Container} from 'reactstrap';
+import Navbar from "../component/Nav";
 
 const WriteBlog = () =>{
     return (
         <div className="writting">
-            <header>
-                <NavHead></NavHead>
-            </header>
+            <Navbar/>
             <Container>
                 <h1>Blog</h1>
                 <AddPost></AddPost>
