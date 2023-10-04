549d680afa98d4d40ff1accc4a5bccccfc96c751
korrakod <korrakod2003@gmail.com>
2023-10-05 00:16:24 +0700

    twentythree

--- src/component/AddPost.jsx
+++ src/component/AddPost.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState ,useContext} from 'react'
+import React, { useRef, useState, useContext } from 'react'
 import { Card, CardBody, Form, Input, Label, Button, Container, FormGroup } from 'reactstrap'
 import "./AddPost.scoped.css"
 import JoditEditor from 'jodit-react';
@@ -6,60 +6,60 @@ import { General } from '../App';
 import axios from 'axios';
 
 function AddPost() {
-    const {supabase_for_use:supabase,session,user} = useContext(General);
-    
+    const { supabase_for_use: supabase, session, user } = useContext(General);
+
     const editor = useRef(null);
     // const [content, setContent] = useState('')
 
-    const[post,setPost] = useState({
-            title:'',
-            content:'',
-            category:'',
+    const [post, setPost] = useState({
+        title: '',
+        content: '',
+        category: '',
     })
-        
+
 
     // fieldChanged function
-    const fieldChanged = (event) =>{
+    const fieldChanged = (event) => {
 
         // console.log(event)
-        setPost({...post,[event.target.name]: event.target.value})
+        setPost({ ...post, [event.target.name]: event.target.value })
 
     }
 
-    const contentFieldChanged = (data) =>{
+    const contentFieldChanged = (data) => {
 
-        setPost({...post,'content':data})
+        setPost({ ...post, 'content': data })
 
     }
 
     // create post function
-    const createPost = (event) =>{
+    const createPost = (event) => {
         // console.log(user?.email)
         console.log(post)
         event.preventDefault();
-       
-        axios.post("http://localhost:3300/creatpost",{
+
+        axios.post("http://localhost:3300/creatpost", {
             title: post.title,
             content: post.content,
             category: post.category,
         })
-        .then(data =>{
-            alert("post created")
-        })
-        .catch((err) => {
-            alert(err)
-        })
-    
+            .then(data => {
+                alert("post created")
+            })
+            .catch((err) => {
+                alert(err)
+            })
+
         // console.log(post)
-        if(post.title.trim()==''){
-           alert('post is required!!')
-           return; 
+        if (post.title.trim() == '') {
+            alert('post is required!!')
+            return;
         }
-        if(post.content.trim()==''){
+        if (post.content.trim() == '') {
             alert('post content is required !!')
             return;
         }
-        if(post.category == ''){
+        if (post.category == '') {
             alert('select some category !!')
             return;
         }
@@ -67,7 +67,7 @@ function AddPost() {
         // submit the form 
         // createPost(post).then(data =>{
         //     alert("post created")
-            // console.log(post)
+        // console.log(post)
         // }).catch((error)=>{
         //     alert('error')
         //     console.log(error)
@@ -81,55 +81,54 @@ function AddPost() {
                     {/* check */}
                     {JSON.stringify(post)}
                     {/* <p>Let's make your blog</p> */}
-                    <Form action='#' onSubmit={createPost}>
-                    <Form onSubmit={createPost}>
-                        <div className="img_cover">
-                            <Input
-                                type="file"
-                                id='Photo'
-                                className='rounded-2'
-                            >
-                            </Input>
-                        </div>
-                        <div className='title__head'>
-                            <Label for='title'>Title</Label>
-                            <Input
-                                type="text"
-                                id='title'
-                                placeholder='Type your title blog...'
-                                className='rounded-2'
-                                name = "title"
-                                onChange={fieldChanged}
-                            />
-                        </div>
+                        <Form onSubmit={createPost}>
+                            <div className="img_cover">
+                                <Input
+                                    type="file"
+                                    id='Photo'
+                                    className='rounded-2'
+                                >
+                                </Input>
+                            </div>
+                            <div className='title__head'>
+                                <Label for='title'>Title</Label>
+                                <Input
+                                    type="text"
+                                    id='title'
+                                    placeholder='Type your title blog...'
+                                    className='rounded-2'
+                                    name="title"
+                                    onChange={fieldChanged}
+                                />
+                            </div>
 
-                        <div className='Catagory'>
-                            <Label for='category'>Select Category</Label>
-                            <Input
-                                type="select"
-                                id='Category'
-                                placeholder='Enter here'
-                                className='rounded-2'
-                                name='category'
-                                onChange={fieldChanged}
-                                defaultValue={0}
-                            >
-                                <option disabled value={0}>--Select category--</option>
-                                <option>
-                                    Decoration
-                                </option>
-                                <option>
-                                    Cooking
-                                </option>
-                                <option>
-                                    Cleaning
-                                </option>
-                                <option>
-                                    Story's DekHor
-                                </option>
-                            </Input>
-                        </div>
-                        {/* <div className="choose">
+                            <div className='Catagory'>
+                                <Label for='category'>Select Category</Label>
+                                <Input
+                                    type="select"
+                                    id='Category'
+                                    placeholder='Enter here'
+                                    className='rounded-2'
+                                    name='category'
+                                    onChange={fieldChanged}
+                                    defaultValue={0}
+                                >
+                                    <option disabled value={0}>--Select category--</option>
+                                    <option>
+                                        Decoration
+                                    </option>
+                                    <option>
+                                        Cooking
+                                    </option>
+                                    <option>
+                                        Cleaning
+                                    </option>
+                                    <option>
+                                        Story's DekHor
+                                    </option>
+                                </Input>
+                            </div>
+                            {/* <div className="choose">
                         <FormGroup>
                             <Label for="exampleFile">
                             Images
@@ -143,37 +142,38 @@ function AddPost() {
                         </FormGroup>
                     </div> */}
 
-                        <div className='content__head'>
-                            <Label for='content'>Content</Label>
-                            <JoditEditor
-                                ref={editor}
-                                value={post.content}
-                                // config={config}
-                                // tabIndex={1} // tabIndex of textarea
-                                onChange={contentFieldChanged}
-                            />
-                            {/* <Input 
+                            <div className='content__head'>
+                                <Label for='content'>Content</Label>
+                                <JoditEditor
+                                    ref={editor}
+                                    value={post.content}
+                                    // config={config}
+                                    // tabIndex={1} // tabIndex of textarea
+                                    onChange={contentFieldChanged}
+                                />
+                                {/* <Input 
                         type="textarea" 
                         id='content'
                         placeholder='Type your content...'
                         className='rounded-2'
                         style={{height:'500px', marginLeft: '1rem', fontSize: '1rem', padding:'1rem'}}
                         /> */}
-                        </div>
+                            </div>
 
-                        <Container className='button__Blog'>
-                            <Button type='submit' className='Post-btn'>Post</Button>
-                            <Button className='Cancel'>Cancel</Button>
-                        </Container>
+                            <Container className='button__Blog'>
+                                <Button type='submit' className='Post-btn'>Post</Button>
+                                <Button className='Cancel'>Cancel</Button>
+                            </Container>
 
-                    </Form>
+                        </Form>
 
                 </CardBody>
 
             </Card>
 
         </div>
-    )
-}
+    
+    );
+};
 
 export default AddPost;
